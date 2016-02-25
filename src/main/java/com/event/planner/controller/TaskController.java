fd1079ea48ee36135b457fb351a74efd38004e8c
shved90 <shvechikov90@gmail.com>
2016-02-26 00:34:03 +0200

    Add exception handler

--- src/main/java/com/event/planner/controller/TaskController.java
+++ src/main/java/com/event/planner/controller/TaskController.java
@@ -3,6 +3,9 @@ package com.event.planner.controller;
 import com.event.planner.model.Task;
 import com.event.planner.service.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.web.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,24 +16,35 @@ import java.util.List;
  * Created by Anton Shvechikov on 23.02.16.
  */
 @RestController
-@RequestMapping("/tasks")
-public class TaskController {
+public class TaskController implements ErrorController {
+
+    private static final String ERROR_PAGE = "error";
 
     @Autowired
     private TaskService taskService;
 
-    @RequestMapping(value = "/add", method = RequestMethod.POST)
+    @RequestMapping(value = "/tasks/add", method = RequestMethod.POST)
     public Task ajaxAddTask(Task task) {
         return taskService.save(task);
     }
 
-    @RequestMapping(method = RequestMethod.GET)
+    @RequestMapping(value = "/tasks", method = RequestMethod.GET)
     public List<Task> ajaxGetTasks() {
         return taskService.getAll();
     }
 
-    @RequestMapping(value = "/remove", method = RequestMethod.POST)
+    @RequestMapping(value = "/tasks/remove", method = RequestMethod.POST)
     public void ajaxRemoveTask(Long taskId) {
         taskService.delete(taskId);
+    }
+
+    @RequestMapping(value = ERROR_PAGE)
+    public ResponseEntity<String> errorResponse() {
+        return new ResponseEntity<>("Attempt to call invalid API method.", HttpStatus.NOT_FOUND);
+    }
+
+    @Override
+    public String getErrorPath() {
+        return ERROR_PAGE;
     }
 }
